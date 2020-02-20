792e5eacf9c713cdaea12d702d4f67cd343aec2c
Sergeyprostoas <Sergeyprostoas@gmail.com>
2020-02-20 23:36:55 +0300

    Finish add style for component

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,44 @@
 <template>
   <div class="container">
-    <span class="h0">This is test</span>
+    <div class="h0">This is test</div>
     <br />
 
-    <span class="h1">This is test</span>
+    <div class="h1">This is test</div>
     <br />
 
-    <span class="h2">This is test</span>
+    <div class="h2">This is test</div>
     <br />
 
-    <span class="p">This is test</span>
+    <div class="p">This is test</div>
     <br />
 
-    <span class="p_small">This is test</span>
+    <div class="p_small">This is test</div>
     <br />
 
-    <span class="p_extra-small">This is test</span>
+    <div class="p_extra-small">This is test</div>
     <br />
 
     <form action="" style="width: 300px">
-      <input class="input" type="text" placeholder="This is test" />
+      <div class="input hover">
+        <div class="input__icon">
+          <img src="~static/img/icon/user-icon.svg" alt="" />
+        </div>
+        <input type="text" placeholder="This is test" />
+      </div>
+      <div class="input imput_without-icon input_darck hover">
+        <input type="text" placeholder="This is test" />
+      </div>
+      <div class="input imput_without-icon input_darck hover">
+        <textarea placeholder="This is test" />
+      </div>
     </form>
     <br />
+
+    <div class="btn btn_primary btn_full-width hover">
+      <div class="btn__icon">
+        <img src="~static/img/icon/login-icon.svg" alt="" />
+      </div>
+      <button>Login</button>
+    </div>
   </div>
 </template>
