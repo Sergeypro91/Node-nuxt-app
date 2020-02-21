bd13102a99314e088c2f68f98f4d808a80e2b861
Sergeyprostoas <Sergeyprostoas@gmail.com>
2020-02-21 23:15:01 +0300

    Finish Login page typeset

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,68 @@
 <template>
-  <div class="container">
-    <div class="h0">This is test</div>
+  <div class="login-page">
+    <div class="login-form">
+      <form action="" class="login-form__wrapper">
+        <div class="login-form__head">
+          <div class="h1 login-form__title">Login</div>
 
-    <div class="h1">This is test</div>
-
-    <div class="h2">This is test</div>
-
-    <div class="p">This is test</div>
-
-    <div class="p_small">This is test</div>
-
-    <div class="p_extra-small">This is test</div>
-
-    <form action="" style="width: 300px">
-      <div class="input hover">
-        <div class="input__icon">
-          <img src="~static/img/icon/user-icon.svg" alt="" />
+          <div class="login-form__addition">
+            <a href="#" class="p_small login-form__sigin">sign up</a>
+            <a href="#" class="p_small login-form__restore-password">
+              forgot password?
+            </a>
+          </div>
         </div>
-        <input type="text" placeholder="This is test" />
-      </div>
-      <div class="input imput_without-icon input_darck hover">
-        <input type="text" placeholder="This is test" />
-      </div>
-      <div class="input imput_without-icon input_darck hover">
-        <textarea placeholder="This is test" />
-      </div>
-    </form>
 
-    <div class="btn hover">
-      <div class="btn__icon">
-        <img src="~static/img/icon/login-icon.svg" alt="" />
+        <div class="input hover">
+          <div class="input__icon">
+            <img src="~static/img/icon/user-icon.svg" alt="" />
+          </div>
+          <input type="text" placeholder="User name" />
+        </div>
+
+        <div class="input hover">
+          <div class="input__icon">
+            <img src="~static/img/icon/password-icon.svg" alt="" />
+          </div>
+          <input type="text" placeholder="Password" />
+        </div>
+
+        <div class="btn btn_primary btn_full-width hover">
+          <div class="btn__icon">
+            <img src="~static/img/icon/login-icon.svg" alt="" />
+          </div>
+          <button>Login</button>
+        </div>
+
+        <div class="p_small login-form__text">
+          Login with one click from social. network:
+        </div>
+        <div class="login-form__social-login">
+          <div class="btn btn_social-login btn_google hover">
+            <div class="btn__icon">
+              <img src="~static/img/icon/google-icon.svg" alt="" />
+            </div>
+          </div>
+          <div class="btn btn_social-login btn_facebook hover">
+            <div class="btn__icon">
+              <img src="~static/img/icon/facebook-icon_white.svg" alt="" />
+            </div>
+          </div>
+          <div class="btn btn_social-login btn_vk hover">
+            <div class="btn__icon">
+              <img src="~static/img/icon/vk-icon_white.svg" alt="" />
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+
+    <div class="slogan-block">
+      <div class="h0">Notes app</div>
+      <div class="h1">notes for you and your friends</div>
+      <div class="h2">
+        Quickly and conveniently create, edit and share your notes, and more...
       </div>
-      <button>Login</button>
     </div>
   </div>
 </template>
---
+++ pages/test.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="h0">This is test</div>
+
+    <div class="h1">This is test</div>
+
+    <div class="h2">This is test</div>
+
+    <div class="p">This is test</div>
+
+    <div class="p_small">This is test</div>
+
+    <div class="p_extra-small">This is test</div>
+
+    <form action="" style="width: 300px">
+      <div class="input hover">
+        <div class="input__icon">
+          <img src="~static/img/icon/user-icon.svg" alt="" />
+        </div>
+        <input type="text" placeholder="This is test" />
+      </div>
+      <div class="input imput_without-icon input_darck hover">
+        <input type="text" placeholder="This is test" />
+      </div>
+      <div class="input imput_without-icon input_darck hover">
+        <textarea placeholder="This is test" />
+      </div>
+    </form>
+
+    <div class="btn hover">
+      <div class="btn__icon">
+        <img src="~static/img/icon/login-icon.svg" alt="" />
+      </div>
+      <button>Login</button>
+    </div>
+  </div>
+</template>
