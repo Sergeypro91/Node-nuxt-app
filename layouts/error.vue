a02884c37e016cd357d40c57c50e4f941fcbcef0
Sergeyprostoas <Sergeyprostoas@gmail.com>
2020-04-15 19:09:49 +0300

    Add 404 page and animation to nuxt routs

---
+++ layouts/error.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="error-layout">
+    <div class="error-layout__header"><div class="h0">404</div></div>
+    <div class="error-layout__footer">
+      <div class="h2">
+        Page whas not found. <n-link to="/notes">Back to the home page!</n-link>
+      </div>
+    </div>
+  </div>
+</template>
